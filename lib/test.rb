1d7766eb971b8f5d19d40d40c3f738633d2ef2c4
Louis Rose <louis.rose@york.ac.uk>
2014-01-08 12:12:38 +0000

    Ensure an error is raised if a test returns anything other than true / false.

--- lib/test.rb
+++ lib/test.rb
@@ -1,5 +1,16 @@
 class Test < Struct.new(:id, :predicate)
   def run(program)
-    eval(program + ";" + predicate) ? :failed : :passed
+    result = eval(program + ";" + predicate)
+    
+    case result
+    when true
+      :passed
+      
+    when false
+      :failed
+    
+    else
+      raise "Error: test returned #{result.inspect}, rather than true or false"
+    end
   end
 end

4c26dda502df40e9928e0aac5dc8410af1eccb2d
Louis Rose <louis.rose@york.ac.uk>
2014-01-14 14:01:21 +0000

    Fix driver to work with new CommandLine API.

--- lib/mutiny/driver.rb
+++ lib/mutiny/driver.rb
@@ -1,6 +1,6 @@
 require_relative "command_line"
 
-test_suite = ARGV[0]
-puts "Test suite: #{test_suite}"
+test_suite_path = ARGV[0]
+puts "Test suite path: #{test_suite_path}"
 
-puts CommandLine.new(test_suite, noisy: true).run
+puts Mutiny::CommandLine.new(test_suite_path, noisy: true).run

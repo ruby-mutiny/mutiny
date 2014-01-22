429016bace042d63a2d73628db397800351c01ac
Louis Rose <louis.rose@york.ac.uk>
2014-01-22 15:47:11 +0000

    Tidy up mutiny step definitions by using Cucumber's map_headers! and map_columns! helpers for Table.

--- features/step_definitions/mutiny_steps.rb
+++ features/step_definitions/mutiny_steps.rb
@@ -8,13 +8,15 @@ end
 Then(/^I should receive the following results:$/) do |expected_results|
   @results.length.should eq(expected_results.hashes.length)
 
+  expected_results.map_headers! { |header| header.downcase.to_sym }
+  expected_results.map_column!("Line") { |v| v.to_i }
+  expected_results.map_column!("Change") { |v| v.to_sym }
+  expected_results.map_column!("Result") { |v| v.to_sym }
+
   expected_results.hashes.each do |row|
-    line = row['Line'].to_i
-    change = row['Change'].to_sym
-    
-    actual = @results.for(line, change).killed? ? :killed : :alive
-    expected = row['Result'].to_sym
-    message = "expected the mutant on line #{line} with change #{change} to be #{expected}, but it was #{actual}"
+    actual = @results.for(row[:line], row[:change]).killed? ? :killed : :alive
+    expected = row[:result]
+    message = "expected the mutant on line #{row[:line]} with change #{row[:change]} to be #{expected}, but it was #{actual}"
     
     expect(actual).to eq(expected), message
   end

2adc669d55c77812bed34ce0884bbddba2f466f2
Louis Rose <louismrose@gmail.com>
2014-03-28 14:44:53 +0000

    Updated binary to help with non-incremental mutator experimentation.

--- lib/mutiny/driver.rb
+++ lib/mutiny/driver.rb
@@ -1,6 +1,18 @@
 require_relative "command_line"
+require_relative "mutator/command_line"
 
-test_suite_path = ARGV[0]
-puts "Test suite path: #{test_suite_path}"
+mode = ARGV[0]
 
-puts Mutiny::CommandLine.new(test_suite_path, noisy: true).run
+if mode == "mutate"
+  mutants = Mutiny::Mutator::CommandLine.new(path: ARGV[1], operator: "*", noisy: true).run
+
+  mutants.each do |mutant|
+    puts "#{mutant.operator}(#{mutant.change}) at #{mutant.path}@#{mutant.position}"
+  end
+
+else
+  test_suite_path = ARGV[0]
+  puts "Test suite path: #{test_suite_path}"
+
+  puts Mutiny::CommandLine.new(test_suite_path, noisy: true).run
+end

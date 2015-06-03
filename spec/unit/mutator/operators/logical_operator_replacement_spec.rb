55439c493fbbf9a9de7213f28abf9b95fe48f70f
Louis Rose <louismrose@gmail.com>
2015-06-03 11:07:08 +0100

    Add tests for LOR and ROR operators.

---
+++ spec/unit/mutator/operators/logical_operator_replacement_spec.rb
@@ -0,0 +1,21 @@
+module Mutiny
+  module Mutator
+    module Operators
+      describe LogicalOperatorReplacement do
+        it "mutates each operator to every other operator" do
+          operators = %i(& | ^)
+
+          operators.each do |original_operator|
+            program = "foo #{original_operator} bar"
+            mutants = subject.mutate(program)
+            expected_mutants = operators
+                               .reject { |op| op == original_operator }
+                               .map { |op| program.gsub(original_operator.to_s, op.to_s) }
+
+            expect(mutants).to eq(expected_mutants)
+          end
+        end
+      end
+    end
+  end
+end
